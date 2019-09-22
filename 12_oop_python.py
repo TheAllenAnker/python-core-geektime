2fc682b4a8c2149376c74545b8a4e4146e19d5f6
TheAllenAnker <theallenanker@gmail.com>
2019-09-22 21:33:04 +0800

    Learned some OOP practices in python.

---
+++ 12_oop_python.py
@@ -0,0 +1,46 @@
+# Author: Allen Anker
+# Created by Allen Anker on 2019-09-22
+
+import pylru
+
+
+class SearchBaseEngine():
+    def __init__(self):
+        pass
+
+    def search(self, query):
+        pass
+
+
+class BOWInvertedIndexEngine(SearchBaseEngine):
+    pass
+
+
+class LRUCache(object):
+    def __init__(self, size=32):
+        self.cache = pylru.lrucache(size)
+
+    def has(self, key):
+        return key in self.cache
+
+    def get(self, key):
+        return self.cache[key]
+
+    def set(self, key, value):
+        self.cache[key] = value
+
+
+class BOWInvertedIndexEngineWithCache(BOWInvertedIndexEngine, LRUCache):
+    def __init__(self):
+        super(BOWInvertedIndexEngineWithCache, self).__init__()
+        LRUCache.__init__(self)
+
+    def search(self, query):
+        if self.has(query):
+            print('cache hit!')
+            return self.get(query)
+
+        result = super(BOWInvertedIndexEngineWithCache, self).search(query)
+        self.set(query, result)
+
+        return result
